dd1535461bad75a12205393163381b5d14b47f74
MasatoMakino <makino.masato.g@gmail.com>
2022-06-13 10:38:14 +0900

    ignore : bin

--- bin/Bundler.d.ts
+++
@@ -1,8 +0,0 @@
-import { Option } from "./Option";
-interface BundlerSet {
-    bundleDevelopment: Function;
-    watchBundle: Function;
-}
-export declare function getBundlerSet(option: Option): BundlerSet;
-export {};
-//# sourceMappingURL=Bundler.d.ts.map
--- bin/Clean.d.ts
+++
@@ -1,3 +0,0 @@
-import { Option } from "./Option";
-export declare function getCleanTask(option: Option): () => Promise<void>;
-//# sourceMappingURL=Clean.d.ts.map
