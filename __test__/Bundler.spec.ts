6b875b35fe3d4e5b787e016718be2c29a15e5b7d
MasatoMakino <makino.masato.g@gmail.com>
2023-06-12 20:56:03 +0900

    add : test for bundle/development

--- __test__/Bundler.spec.ts
+++ __test__/Bundler.spec.ts
@@ -1,3 +1,5 @@
+import fs from "fs";
+import path from "path";
 import { getBundlerSet } from "../src/Bundler";
 import { initOptions } from "../src/Option";
 
@@ -6,10 +8,24 @@ describe("Bundler", () => {
     const option = initOptions(null);
     return getBundlerSet(option);
   };
+  const isExistFile = (relativePath: string) => {
+    const isExist = fs.existsSync(path.resolve(process.cwd(), relativePath));
+    expect(isExist).toBeTruthy();
+  };
+
   test("getBundlerSet", () => {
     const bundlerSet = getDefaultBundlerSet();
     expect(bundlerSet.bundleDevelopment).toBeTruthy();
     expect(bundlerSet.watchBundle).toBeTruthy();
+  });
+
+  test("bundleDevelopment:default", async () => {
+    const bundlerSet = getDefaultBundlerSet();
+    await bundlerSet.bundleDevelopment();
+    isExistFile("./docs/demo/demo.js");
+    isExistFile("./docs/demo/demoTypeScript.js");
+    isExistFile("./docs/demo/sub/demoSub.js");
+    isExistFile("./docs/demo/vendor.js");
   });
 
   test("watch", () => {
