f470ba4174a84f51b68fd5c66e719d953722897d
MasatoMakino <makino.masato.g@gmail.com>
2023-06-16 15:45:44 +0900

    refactor : not exist

--- __test__/Util.ts
+++ __test__/Util.ts
@@ -1,7 +1,12 @@
 import fs from "fs";
 import path from "path";
 
+function isExist(relativePath: string): boolean {
+  return fs.existsSync(path.resolve(process.cwd(), relativePath));
+}
 export function isExistFile(relativePath: string): void {
-  const isExist = fs.existsSync(path.resolve(process.cwd(), relativePath));
-  expect(isExist).toBeTruthy();
+  expect(isExist(relativePath)).toBeTruthy();
+}
+export function isNotExistFile(relativePath: string): void {
+  expect(isExist(relativePath)).toBeFalsy();
 }
