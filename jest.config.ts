75fb0d15f3992ca0a7164c6d21d8635a4b74e24f
MasatoMakino <makino.masato.g@gmail.com>
2023-07-23 15:28:04 +0900

    fix : extensions field
    JestConfigWithTsJestプリセットに含まれている

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,6 @@ import type { JestConfigWithTsJest } from "ts-jest";
 const jestConfig: JestConfigWithTsJest = {
   clearMocks: true,
   preset: "ts-jest/presets/default-esm",
-  extensionsToTreatAsEsm: [".ts"],
   moduleNameMapper: {
     "^(\\.{1,2}/.*)\\.js$": "$1",
   },
