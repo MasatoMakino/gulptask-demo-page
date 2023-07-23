c9d2b2f14d636e4251e3941d03446f9c4e136caa
MasatoMakino <makino.masato.g@gmail.com>
2023-07-23 14:07:47 +0900

    fix : jest.config.ts
    https://kulshekhar.github.io/ts-jest/docs/next/guides/esm-support/

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,15 @@
-
 export default {
   clearMocks: true,
   preset: "ts-jest",
   testEnvironment: "node",
+  extensionsToTreatAsEsm: [".ts"],
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
   transform: {
-    "\\.tsx?$": ["ts-jest", { tsconfig: "./__test__/tsconfig.json" }],
+    "\\.tsx?$": [
+      "ts-jest",
+      { tsconfig: "./__test__/tsconfig.json", useESM: true },
+    ],
   },
 };
