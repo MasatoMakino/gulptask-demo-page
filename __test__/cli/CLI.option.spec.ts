8a5d7fe76b55b2bbf4979e397bc9254b9629009c
MasatoMakino <makino.masato.g@gmail.com>
2025-05-08 13:57:38 +0900

    test: add additional CLI option tests for srcDir, distDir, body, style, copyTargets, externalScripts, prefix, rule, compileTarget, and compileModule

--- __test__/cli/CLI.option.spec.ts
+++ __test__/cli/CLI.option.spec.ts
@@ -36,6 +36,110 @@ describe("CLI Command Tests", () => {
     },
   );
 
+  test("srcDirが渡される", async () => {
+    process.argv = ["node", "CLI.ts", "--srcDir", "testSrc"];
+    await runCommand();
+    const calledArgs = mockGenerateTasks.mock.calls[0][0];
+    expect(calledArgs).toEqual({
+      srcDir: "testSrc",
+    });
+  });
+
+  test("distDirが渡される", async () => {
+    process.argv = ["node", "CLI.ts", "--distDir", "testDist"];
+    await runCommand();
+    const calledArgs = mockGenerateTasks.mock.calls[0][0];
+    expect(calledArgs).toEqual({
+      distDir: "testDist",
+    });
+  });
+  test("bodyが渡される", async () => {
+    process.argv = ["node", "CLI.ts", "--body", "testBody"];
+    await runCommand();
+    const calledArgs = mockGenerateTasks.mock.calls[0][0];
+    expect(calledArgs).toEqual({
+      body: "testBody",
+    });
+  });
+
+  test("styleが渡される", async () => {
+    process.argv = ["node", "CLI.ts", "--style", "testStyle"];
+    await runCommand();
+    const calledArgs = mockGenerateTasks.mock.calls[0][0];
+    expect(calledArgs).toEqual({
+      style: "testStyle",
+    });
+  });
+
+  test("copyTargetsが渡される", async () => {
+    process.argv = ["node", "CLI.ts", "--copyTargets", "png", "jpg", "jpeg"];
+    await runCommand();
+    const calledArgs = mockGenerateTasks.mock.calls[0][0];
+    expect(calledArgs).toEqual({
+      copyTargets: ["png", "jpg", "jpeg"],
+    });
+  });
+
+  test("externalScriptsが渡される", async () => {
+    process.argv = [
+      "node",
+      "CLI.ts",
+      "--externalScripts",
+      "https://code.createjs.com/1.0.0/createjs.min.js",
+    ];
+    await runCommand();
+    const calledArgs = mockGenerateTasks.mock.calls[0][0];
+    expect(calledArgs).toEqual({
+      externalScripts: ["https://code.createjs.com/1.0.0/createjs.min.js"],
+    });
+  });
+
+  test("prefixが渡される", async () => {
+    process.argv = ["node", "CLI.ts", "--prefix", "testPrefix"];
+    await runCommand();
+    const calledArgs = mockGenerateTasks.mock.calls[0][0];
+    expect(calledArgs).toEqual({
+      prefix: "testPrefix",
+    });
+  });
+
+  test("ruleが渡される", async () => {
+    process.argv = ["node", "CLI.ts", "--rule", "./rule.js"];
+    await runCommand();
+    const calledArgs = mockGenerateTasks.mock.calls[0][0];
+    expect(calledArgs).toEqual({
+      rule: "./rule.js",
+      rules: [
+        {
+          test: /\.(vert|frag|glsl)$/,
+          use: [
+            {
+              loader: "webpack-glsl-loader",
+            },
+          ],
+        },
+      ],
+    });
+  });
+
+  test("--compileTargetが渡される", async () => {
+    process.argv = ["node", "CLI.ts", "--compileTarget", "es5"];
+    await runCommand();
+    const calledArgs = mockGenerateTasks.mock.calls[0][0];
+    expect(calledArgs).toEqual({
+      compileTarget: "es5",
+    });
+  });
+
+  test("--compileModuleが渡される", async () => {
+    process.argv = ["node", "CLI.ts", "--compileModule", "es2020"];
+    await runCommand();
+    const calledArgs = mockGenerateTasks.mock.calls[0][0];
+    expect(calledArgs).toEqual({
+      compileModule: "es2020",
+    });
+  });
+
   test("--compileModuleResolutionが渡される", async () => {
     process.argv = ["node", "CLI.ts", "--compileModuleResolution", "node"];
     await runCommand();
